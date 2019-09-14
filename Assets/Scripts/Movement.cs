09a82c8491172592e4fc6fa8f9cb2af6bc59b5a3
Daniel Liu <hi.daniel.liu@gmail.com>
2019-09-14 12:26:48 -0400

    added primitive movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private double goal_velocity;
+    public float MAX_SPEED;
+    public float ACCELERATION;
+
+    private Rigidbody rb;
+
+    private float x_default;
+    private float y_default;
+
+    private Vector3 goal_velocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        goal_velocity = 0;
+        rb = gameObject.GetComponent<Rigidbody>();
+        goal_velocity = Vector3.zero;
+
+        x_default = Input.GetAxis("Horizontal");
+        y_default = Input.GetAxis("Vertical");
     }
 
     // FixedUpdate is called before each physics frame
     void FixedUpdate()
     {
-        
+        float x_input = Input.GetAxis("Horizontal") - x_default;
+        float y_input = Input.GetAxis("Vertical") - y_default;
+
+        float input_size = Mathf.Sqrt(x_input * x_input + y_input * y_input);
+        if (input_size > 1)
+        {
+            x_input /= input_size;
+            y_input /= input_size;
+        }
+
+        goal_velocity = MAX_SPEED * new Vector3(x_input, y_input, 0);
+
+        // Temporary primitive movement method
+        rb.velocity = goal_velocity;
     }
 
     // Update is called once per frame
