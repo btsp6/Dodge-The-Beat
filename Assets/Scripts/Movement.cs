cbd91a68a41b56012542320211d852967696f088
Daniel Liu <hi.daniel.liu@gmail.com>
2019-09-14 11:46:55 -0400

    added movement.cs

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private double goal_velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goal_velocity = 0;
+    }
+
+    // FixedUpdate is called before each physics frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
