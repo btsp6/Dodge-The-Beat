f7f97a24091a74325700085a5d775040632c791b
Daniel Liu <hi.daniel.liu@gmail.com>
2019-09-14 13:34:44 -0400

    spawned bars

--- Assets/Scripts/BarSpawnerScript.cs
+++ Assets/Scripts/BarSpawnerScript.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class BarSpawnerScript : MonoBehaviour
 {
     public GameObject bar_prefab;
+    public int NUMBER_OF_BARS;
+    public int RADIUS;
 
+    private GameObject[] bar_list;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        bar_list = new GameObject[NUMBER_OF_BARS];
+        for(int i = 0; i < NUMBER_OF_BARS; i++)
+        {
+            bar_list[i] = (GameObject)Instantiate(
+                bar_prefab, 
+                RADIUS * new Vector3(Mathf.Cos(2*Mathf.PI*i/NUMBER_OF_BARS), Mathf.Sin(2 * Mathf.PI * i / NUMBER_OF_BARS), 0), 
+                Quaternion.Euler(0, 0, 360.0F*i/NUMBER_OF_BARS));
+        }
     }
 
     // Update is called once per frame
@@ -17,4 +29,5 @@ public class BarSpawnerScript : MonoBehaviour
     {
         
     }
+
 }
