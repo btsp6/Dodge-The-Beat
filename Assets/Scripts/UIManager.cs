fc0cd5790501850b7d8513a1ef3fabaea2ea120d
tong <tongzhao997@gmail.com>
2019-09-15 03:50:42 -0400

    scrollable button scripting works

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -19,32 +19,4 @@ public class UIManager : MonoBehaviour
         // Debug.Log("help2");
         SceneManager.LoadScene("Game"); 
     }
-
-    public void LogStreamingAssets(){
-        string myPath = Application.streamingAssetsPath;
-
-        Debug.Log(myPath);
-        DirectoryInfo dir = new DirectoryInfo(myPath);
-        FileInfo[] info = dir.GetFiles("*.*");
-        
-        Debug.Log(info.Length/2);
-        string[] fileNames = new string[info.Length/2];
-        int j = 0;
-
-        for (int i=0; i< info.Length; i++){
-            string fileName = info[i].Name;
-            string maybeIsMeta = fileName.Substring(fileName.Length - 5, 5);
-            bool isMeta = (maybeIsMeta.Equals(".meta"));
-            if (!isMeta){
-                // do stuff
-                Debug.Log(fileName);
-                fileNames[j] = fileName;
-                j++;
-            }            
-        }
-        
-        Debug.Log(fileNames.ToString());
-        // fileNames.ToList().ForEach(name => Debug.Log(name.ToString()));
-    }
-    
 }
