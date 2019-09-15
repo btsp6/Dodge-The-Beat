acfb4a0bb180f24a6e6f392c46a1ed68d947d051
Daniel Liu <hi.daniel.liu@gmail.com>
2019-09-14 23:54:53 -0400

    added walls

--- Assets/Scripts/BarControllerScript.cs
+++ Assets/Scripts/BarControllerScript.cs
@@ -7,9 +7,11 @@ using EzySlice;
 public class BarControllerScript : MonoBehaviour
 {
     public GameObject bar_prefab;
+    public GameObject wall_prefab;
     public GameObject sliced_object;
     public GameObject masker_prefab;
     public const int NUMBER_OF_BARS = 10;
+    public const int NUMBER_OF_WALLS = 64;
     public float INITIAL_RADIUS;
     public float DELTA_RADIUS;
 
@@ -17,9 +19,10 @@ public class BarControllerScript : MonoBehaviour
     private GameObject masker;
     private GameObject bar;
     private GameObject[] bar_list;
+    private GameObject[] walls;
 
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +32,7 @@ public class BarControllerScript : MonoBehaviour
         SliceBars();
 
         bar_list = new GameObject[NUMBER_OF_BARS];
+        walls = new GameObject[NUMBER_OF_WALLS];
         for(int i = 0; i < NUMBER_OF_BARS; i++)
         {
             bar_list[i] = (GameObject)Instantiate(
@@ -37,6 +41,16 @@ public class BarControllerScript : MonoBehaviour
                 Quaternion.Euler(0, 0, 360F*i/NUMBER_OF_BARS));
             bar_list[i].GetComponent<Transform>().localScale = new Vector3(0.1F, 1, 1);
             bar_list[i].GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.red);
+            
+        }
+        for(int i = 0; i < NUMBER_OF_WALLS; i++)
+        {
+
+            walls[i] = (GameObject)Instantiate(
+                wall_prefab,
+                Quaternion.Euler(0, 0, 360F * i / NUMBER_OF_WALLS) * ((0.5F + radius) * Vector3.right),
+                Quaternion.Euler(0, 0, 360F * i / NUMBER_OF_WALLS));
+            walls[i].GetComponent<Renderer>().enabled = false;
         }
 
         bar.GetComponent<Renderer>().enabled = false;
@@ -59,6 +73,10 @@ public class BarControllerScript : MonoBehaviour
             SetBarHeight(i, MusicConversionScript.reducedData[i]*50);
         }
         masker.GetComponent<Transform>().localScale = new Vector3(2 * radius, 2 * radius, 1);
+        for(int i = 0; i < NUMBER_OF_WALLS; i++)
+        {
+            walls[i].GetComponent<Transform>().position = Quaternion.Euler(0, 0, 360F * i / NUMBER_OF_WALLS) * ((0.5F + radius) * Vector3.right);
+        }
     }
 
     void SliceBars()
@@ -76,7 +94,6 @@ public class BarControllerScript : MonoBehaviour
 
     void SetBarHeight(int index, float size)
     {
-        // bar_list[index].GetComponent<Transform>().position = Quaternion.Euler(0, 0, 360F * index / NUMBER_OF_BARS) * ((radius + size / 2) * Vector3.right);
         bar_list[index].GetComponent<Transform>().localScale = new Vector3((1 + DELTA_RADIUS*size) * 2*radius, (1 + DELTA_RADIUS * size) * 2*radius, 1);
         bar_list[index].GetComponent<Renderer>().material.SetColor("_EmissionColor", new Color(1, size*size, size*size, 1));
     }
